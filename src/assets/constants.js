bd9bb9267c49c32a801b1d9a4932249a9d98dc18
Shanyik <tkrisi213@gmail.com>
2024-05-23 17:31:57 +0200

    feat: deleted unnecessary files, about page done

---
+++ src/assets/constants.js
@@ -0,0 +1,40 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+} from "../assets";
+
+export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+
+ export const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Angular Developer",
+      icon: creator,
+    },
+  ];
