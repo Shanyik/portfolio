f2e6143ba0f73a71122bb018b1f2daad7fc3e047
Shanyik <tkrisi213@gmail.com>
2024-05-23 20:47:34 +0200

    feat: experience component done

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -3,6 +3,10 @@ import {
   backend,
   creator,
   web,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
 } from "../assets";
 
 export const navLinks = [
@@ -36,5 +40,60 @@ export const navLinks = [
     {
       title: "Angular Developer",
       icon: creator,
+    },
+  ];
+
+ export const experiences = [
+    {
+      title: "Lorem ipsum",
+      company_name: "dolor sit amet",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum ornare magna eu dignissim.",
+        "Morbi eu mattis metus, vel scelerisque sapien.",
+        "Fusce convallis laoreet mauris, a molestie neque. Integer imperdiet bibendum bibendum.",
+        "Donec malesuada turpis ac velit dictum varius. Ut hendrerit elit et mauris feugiat, a rutrum augue viverra.",
+      ],
+    },
+    {
+      title: "Lorem ipsum",
+      company_name: "dolor sit amet",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum ornare magna eu dignissim.",
+        "Morbi eu mattis metus, vel scelerisque sapien.",
+        "Fusce convallis laoreet mauris, a molestie neque. Integer imperdiet bibendum bibendum.",
+        "Donec malesuada turpis ac velit dictum varius. Ut hendrerit elit et mauris feugiat, a rutrum augue viverra.",
+      ],
+    },
+    {
+      title: "Lorem ipsum",
+      company_name: "dolor sit amet",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum ornare magna eu dignissim.",
+        "Morbi eu mattis metus, vel scelerisque sapien.",
+        "Fusce convallis laoreet mauris, a molestie neque. Integer imperdiet bibendum bibendum.",
+        "Donec malesuada turpis ac velit dictum varius. Ut hendrerit elit et mauris feugiat, a rutrum augue viverra.",
+      ],
+    },
+    {
+      title: "Lorem ipsum",
+      company_name: "dolor sit amet",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum ornare magna eu dignissim.",
+        "Morbi eu mattis metus, vel scelerisque sapien.",
+        "Fusce convallis laoreet mauris, a molestie neque. Integer imperdiet bibendum bibendum.",
+        "Donec malesuada turpis ac velit dictum varius. Ut hendrerit elit et mauris feugiat, a rutrum augue viverra.",
+      ],
     },
   ];
